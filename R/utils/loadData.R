d426c95bc2578afb7d26f56dc56ec72eeeaad5b6
sturmti <TimoSturm@Sturm-MacBook-Pro.local>
2017-04-09 18:33:29 +0200

    improved variable naming

--- R/utils/loadData.R
+++ R/utils/loadData.R
@@ -1,12 +1,17 @@
 if (!require(data.table)) install.packages("data.table")
 
 ### Import Data
-###   - dataTrain: import of train.csv
-###   - dataItems: import of items.csv
-###   - dataClass: import class.csv
-dataTrain <- data.table(read.csv(file = "/Users/TimoSturm/Documents/Masterstudium/2. Semester/Data Mining II/Data Mining Cup 2017/DMC_2017_task/train.csv", header = T, sep = "|"))
-dataItems <- data.table(read.csv(file = "/Users/TimoSturm/Documents/Masterstudium/2. Semester/Data Mining II/Data Mining Cup 2017/DMC_2017_task/items.csv", header = T, sep = "|"))
-dataClass <- data.table(read.csv(file = "/Users/TimoSturm/Documents/Masterstudium/2. Semester/Data Mining II/Data Mining Cup 2017/DMC_2017_task/class.csv", header = T, sep = "|"))
+###   - data.train: import of train.csv
+###   - data.items: import of items.csv
+###   - data.class: import class.csv
+data.train <- data.table(read.csv(file = "/Users/TimoSturm/Documents/Masterstudium/2. Semester/Data Mining II/Data Mining Cup 2017/DMC_2017_task/train.csv", header = T, sep = "|"))
+data.items <- data.table(read.csv(file = "/Users/TimoSturm/Documents/Masterstudium/2. Semester/Data Mining II/Data Mining Cup 2017/DMC_2017_task/items.csv", header = T, sep = "|"))
+data.class <- data.table(read.csv(file = "/Users/TimoSturm/Documents/Masterstudium/2. Semester/Data Mining II/Data Mining Cup 2017/DMC_2017_task/class.csv", header = T, sep = "|"))
 
+### factorize the polynominal columns
+##    - data.items: all columns
+##    - data.train:
+##    - data.class:
 
-dataTrain
+data.items <- factor(data.items)
+
