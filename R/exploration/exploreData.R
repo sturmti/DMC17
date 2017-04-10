b42515e463d6f7b6c348ca72f363789b099033c9
sturmti <TimoSturm@Sturm-MacBook-Pro.local>
2017-04-10 23:03:03 +0200

    small changes

--- R/exploration/exploreData.R
+++ R/exploration/exploreData.R
@@ -1,7 +1,10 @@
 source("utils/loadData.R")
+source("preprocessing/featureEngineering.R")
+source("utils/initializeData.R")
 library(ggplot2)
 
 data.items <- getItemData()
 data.train <- getTrainData()
 data.class <- getClassData()
+
 
--- R/preprocessing/featureEngineering.R
+++ R/preprocessing/featureEngineering.R
@@ -1,11 +1,7 @@
-### Feature Engineering
-## Purpose: Try out some things (but don't delete it to document it! (if it's not trivial))
-##          If something meaningful was found and should be available in the main train data, move it to featureGeneration.R.
-
 #' Create additional engineered features for the train.csv
 #' @description Computes and adds the engineered features to the data.train data set.
 #' @return A data.table containing the enhanced data.train data set.
-createEngineeredFeatures <- function(data.train){
+createEngineeredFeaturesForDataTrain <- function(data.train){
   ## based on price, competitorPrice
   # diff and ratios of diff between price, competitorPrice
   data.train$diffPriceCompetitorPrice <- data.train$price - data.train$competitorPrice
