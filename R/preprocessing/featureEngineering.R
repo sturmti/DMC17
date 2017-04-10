07293bf3959c2ef7c1187940bfba9bc2aef38a1c
sturmti <timo.sturm@googlemail.com>
2017-04-10 23:32:04 +0200

    added the feature quantity

--- R/preprocessing/featureEngineering.R
+++ R/preprocessing/featureEngineering.R
@@ -2,6 +2,16 @@
 #' @description Computes and adds the engineered features to the data.train data set.
 #' @return A data.table containing the enhanced data.train data set.
 createEngineeredFeaturesForDataTrain <- function(data.train){
+  ## based on click, basket, order
+  # aggregates the features click, baset, order into one single feature
+  data.train <- within(data.train, {
+    actionType = ifelse(data.train$click == 1, "click", ifelse(data.train$basket == 1, "basket", "order"))
+  })
+  ## based on revenue, price
+  # quantity of ordered products, NA if action is click or basket
+  data.train <- within(data.train, {
+    quantity = ifelse(data.train$order == 1, (data.train$revenue / data.train$price), NA)
+  })
   ## based on price, competitorPrice
   # diff and ratios of diff between price, competitorPrice
   data.train$diffPriceCompetitorPrice <- data.train$price - data.train$competitorPrice
