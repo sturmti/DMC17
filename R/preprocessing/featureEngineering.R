17a6ec730bde4c47a15ab665ec138801014ef162
NancyKunath <lori_NK@web.de>
2017-04-30 17:41:15 +0200

    Add productActionCounter

--- R/preprocessing/featureEngineering.R
+++ R/preprocessing/featureEngineering.R
@@ -55,6 +55,9 @@ createEngineeredFeaturesForDataTrain <- function(data.train){
   # mean price per product
   meanPricePerProduct <- setNames(aggregate(x = data.train$price, by = list(pid = data.train$pid), FUN = mean), c("pid", "meanPricePerProduct"))
   data.train <- data.table(left_join(data.train, data.table(meanPricePerProduct), by=c("pid" = "pid")))
+  # number of transactions per product
+  countPerProduct <- setNames(aggregate(x = data.train$pid, by = list(pid = data.train$pid), FUN = length), c("pid", "productActionCounter"))
+  data.train <- data.table(left_join(data.train, data.table(countPerProduct), by=c("pid" = "pid")))
   
   data.train
 }
